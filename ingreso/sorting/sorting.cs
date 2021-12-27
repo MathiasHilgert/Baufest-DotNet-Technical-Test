6b1a768b84fbf07396c031a66e95eb8283891f67
Jenkins <jenkins@BFDEV379.baunet.local>
2021-12-27 14:57:59 -0300

    Commit inicial del examen

---
+++ ingreso/sorting/sorting.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ingreso.Sorting
+{
+    public class Sorting
+    {
+
+        /**
+         * Se debe ordenar primero por puntuación de manera descendente, luego por nombre de manera ascendente.
+         * @param jugadores la lista de jugadores a ordenar
+         * @return la lista ordenada de jugadores
+         */
+        public static List<Jugador> OrdenarPorPuntuacionYNombre(List<Jugador> jugadores)
+        {
+            //TODO: resolver
+            throw new NotImplementedException();
+        }
+
+        /**
+         * Se debe ordenar primero por puntuación de manera descendente. Cuando 2 jugadores tienen igual cantidad de puntos,
+         * el que tiene menos perdidas se lo considerara el mayor. Luego a igual puntos y perdidas se seguirá usando el
+         * nombre de manera ascendente.
+         * @param jugadores la lista de jugadores a ordenar
+         * @return la lista ordenada de jugadores
+         */
+        public static List<Jugador> OrdenarPorPuntuacionPerdidasYNombre(List<Jugador> jugadores)
+        {
+            //TODO: resolver
+            throw new NotImplementedException();
+        }
+    }
+}
